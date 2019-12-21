2d631e9f8a82115c127e7ede0619dc5b2c74a3f4
baha312 <bahtiyarbolotbekov@gmail.com>
2019-12-21 11:50:12 +0600

    first commit

---
+++ task10.py
@@ -0,0 +1,10 @@
+string_ = "KYRGYZSTAN"
+print(string_[2])
+print(string_[1:])
+print(string_[:6])
+print(string_[8:])
+print(string_[1::2])
+print(string_[0::2])
+print(string_[::-1])
+print(string_[::-1][::2])
+print(len(string_))
